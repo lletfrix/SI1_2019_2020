eee3049627a37d5a7ee73ecd548f7739d8242d67
Rafael Sánchez <lletfrix@gmail.com>
2019-10-03 16:51:41 +0200

    inital 2nd assignment commit

---
+++ 2p/app/routes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from app import app
+from flask import render_template, request, url_for, redirect, session
+import json
+import os
+import sys
+
+@app.route('/')
+def index():
+    return render_template('index.html')
