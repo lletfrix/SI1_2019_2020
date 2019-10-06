6dbf322782ddaba467e5611a3d34e6093406a6c4
Rafael Sánchez <lletfrix@gmail.com>
2019-10-06 23:17:22 +0200

    Start search functionality

--- 2p/app/routes.py
+++ 2p/app/routes.py
@@ -7,10 +7,27 @@ import json
 import os
 import sys
 
-@app.route('/')
-@app.route('/index.html')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index.html', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    movies = None
+    if request.method == 'POST':
+        # Get the search result
+        title = request.form.get('title')
+        filter = request.form.get('filter')
+        with open(os.path.join(app.root_path,'catalogue/catalogue.json'), encoding="utf-8") as data_file:
+            catalogue = json.loads(data_file.read())
+            movies = catalogue['peliculas']
+            # movies = list(filter(, movies))
+        return render_template('index.html', title='Búsqueda', movies=movies)
+        
+    else:
+        # Get the last movies
+        with open(os.path.join(app.root_path,'catalogue/catalogue.json'), encoding="utf-8") as data_file:
+            catalogue = json.loads(data_file.read())
+            movies = catalogue['peliculas']
+            # movies = list(filter(, movies))
+        return render_template('index.html', title='Home', movies=movies)
 
 @app.route('/register', methods=['GET'])
 def register():
