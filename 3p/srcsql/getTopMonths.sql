2a8c19aa6a17bfa75137ec80ef016ca72afd6a52
Alejandro Santorum <alejandro.santorum@gmail.com>
2019-11-08 20:38:27 +0100

    improve getTopMonths function

--- 3p/srcsql/getTopMonths.sql
+++ 3p/srcsql/getTopMonths.sql
@@ -11,36 +11,36 @@
 -- ARG 2 : threshold for totalamount
 CREATE OR REPLACE FUNCTION getTopMonths(integer, integer)
     RETURNS TABLE (
-        m integer,
-        y integer,
-        t_prod bigint,
-        t_cash numeric
+        month integer,
+        year integer,
+        total_prod bigint,
+        total_cash numeric
     )
     AS $$
     BEGIN
         RETURN QUERY SELECT
-            CAST(month AS integer),
-            CAST(year AS integer),
-            total_prod,
-            total_cash
+            CAST(m AS integer),
+            CAST(yr AS integer),
+            t_prod,
+            t_cash
         FROM (
-                SELECT DATE_PART('month', orderdate) as month,
-                       DATE_PART('year', orderdate) as year,
-                       sum(quantity) as total_prod
+                SELECT DATE_PART('month', orderdate) as m,
+                       DATE_PART('year', orderdate) as yr,
+                       sum(quantity) as t_prod
                 FROM orders INNER JOIN orderdetail
                     ON orders.orderid=orderdetail.orderid
-                GROUP BY month, year
+                GROUP BY m, yr
             ) TotalProd
         NATURAL JOIN
             (
-                SELECT DATE_PART('month', orderdate) as month,
-                       DATE_PART('year', orderdate) as year,
-                       sum(totalamount) as total_cash
+                SELECT DATE_PART('month', orderdate) as m,
+                       DATE_PART('year', orderdate) as yr,
+                       sum(totalamount) as t_cash
                 FROM orders
-                GROUP BY month, year
+                GROUP BY m, yr
             ) TotalCash
-        WHERE (total_prod > $1)
-             OR (total_cash > $2);
+        WHERE (t_prod > $1)
+             OR (t_cash > $2);
     END;
 $$ LANGUAGE plpgsql;
 
