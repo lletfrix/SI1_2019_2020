ba926f96835790b3fd902488431aa9848d3d0e65
Alejandro Santorum <alejandro.santorum@gmail.com>
2019-11-10 23:32:33 +0100

    add trigger updOrders

---
+++ 3p/srcsql/updOrders.sql
@@ -0,0 +1,30 @@
+CREATE OR REPLACE FUNCTION updOrders_func()
+RETURNS TRIGGER AS $$
+DECLARE
+    nprods integer;
+BEGIN
+    IF TG_OP='INSERT' OR TG_OP='UPDATE' THEN
+        UPDATE orders
+        SET netamount=netamount+(NEW.price*NEW.quantity),
+            orderdate=now()::date
+        WHERE orderid=NEW.orderid;
+        RETURN NEW;
+    ELSE
+        nprods := (SELECT COUNT(*) FROM orderdetail WHERE orderdetail.orderid=OLD.orderid);
+        IF nprods=0 THEN
+            DELETE FROM orders
+            WHERE orders.orderid=OLD.orderid;
+        ELSE
+            UPDATE orders
+            SET netamount=netamount-(OLD.price*OLD.quantity)
+            WHERE orderid=OLD.orderid;
+        END IF;
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER updOrders
+AFTER INSERT OR UPDATE OR DELETE ON orderdetail
+FOR EACH ROW EXECUTE PROCEDURE updOrders_func();
