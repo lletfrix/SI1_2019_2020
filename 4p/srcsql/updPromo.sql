22b1be10ef2dba5384c4d2742addf1d9575b856b
Alejandro Santorum <alejandro.santorum@gmail.com>
2019-12-07 18:14:51 +0100

    fix updPromo trigger function

--- 4p/srcsql/updPromo.sql
+++ 4p/srcsql/updPromo.sql
@@ -18,13 +18,36 @@ CREATE OR REPLACE FUNCTION updPromo_func()
 RETURNS TRIGGER AS $$
     BEGIN
         ----- > TODO: Probably SLEEP HERE !!!!!!!!!!!!!!!!!!!
+
+        -- 1st: Update orderdetail price
         UPDATE orderdetail
-        SET price=products.price*(1-(NEW.promo/100)) -- discount
+        SET price=products.price * (1-(NEW.promo/100)) -- discount
         FROM products, orders, customers
         WHERE orders.customerid=NEW.customerid -- NEW.customerid is equal to customers.customerid
             AND orders.status IS NULL -- cart item
             AND orders.orderid=orderdetail.orderid
             AND orderdetail.prod_id=products.prod_id;
+
+        -- 2nd: Update orders netamount
+        UPDATE orders
+        SET netamount=aux_tb.value
+        FROM (
+            SELECT orderdetail.orderid, sum(orderdetail.price * orderdetail.quantity) as value
+            FROM orders, orderdetail
+            WHERE orders.orderid=orderdetail.orderid
+            GROUP BY orderdetail.orderid
+        ) AS aux_tb
+        WHERE orders.customerid=NEW.customerid
+            AND orders.status IS NULL
+            AND orders.orderid=aux_tb.orderid;
+
+        -- 3rd: Update orders totalamount
+        UPDATE orders
+        SET totalamount=netamount * (1+(tax/100))
+        WHERE customerid=NEW.customerid
+            AND orders.status IS NULL;
+
+    RETURN NULL;
     END;
 $$ LANGUAGE plpgsql;
 
